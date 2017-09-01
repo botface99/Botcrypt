eea17ff4e4f904482bb064a9d0a3e5c362a6127a
Hritik <31449034+botface99@users.noreply.github.com>
2017-09-01 12:49:33 +0530

    Add files via upload

---
+++ Botencryp.py
@@ -0,0 +1,61 @@
+#BOTFACE Encryption Program by hritik
+import os
+botinput=""
+looper="1"
+while looper == '1':
+    s = 'ENCRYPTION DECRYPTION BY BOTFACE'
+    l=s.center(120,'-')
+    print("\n\n",l)
+    botinput = input("\n\n\n\t\tEnter 1 to Encrypt , 0 to Decrypt your selected file : ")   
+    if botinput == '1':
+        filer=''
+        key=''
+        result=''
+        try:
+            filer = input("\n\n\t\tEnter your text file name : ")
+            bot=open(filer,"r")
+        except FileNotFoundError:
+            print("\n\t\tFile not found or enter your filename with absolute path location.")
+        else:
+            content=bot.read()
+            key=input("\n\t\tEnter your key to Encrypt your file : ")
+            v=int(key)
+            s=" "
+            for i in range(0,len(content)):
+                if " " in content[i]:
+                    result=result + s 
+            else:
+                result=result + chr(ord(content[i])-v)
+            bot.close()
+            bot=open(filer,"w")
+            bot.write(result)
+            bot.close()
+            print("\n\t\tYou are successfully Encrypted your selected text file. \n") 
+    elif botinput == '0':
+        filer=''
+        key=''
+        result=''
+        try:
+            filer = input("\n\n\t\tEnter your text file name : ")
+            bot=open(filer,"r")
+        except FileNotFoundError:
+            print("\n\t\tFile not found or enter your filename with absolute path location.")
+        else:
+            content=bot.read()
+            key=input("\n\t\tEnter your key to Decrypt your file : ")
+            v=int(key)
+            s=" "
+            for i in range(0,len(content)):
+                if " " in content[i]:
+                    result=result + s
+            else:
+                result=result + chr(ord(content[i])-v)
+            bot.close()
+            bot=open(filer,"w")
+            bot.write(result)
+            bot.close()
+            print("\n\t\tYou are successfully Decrypted your selected text file. \n") 
+    else:
+        print("\n\n\t\tSomething Wrong \n")
+        
+    
